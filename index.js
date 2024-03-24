f5d21ba74b0402fd5fe8d8a6e6ac064bad7d06d8
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-03-24 13:46:22 +0500

    CLI ATM project

---
+++ index.js
@@ -0,0 +1,58 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "input",
+        name: "userId",
+        message: "Kindly Enter Your Id: ",
+    },
+    {
+        type: "number",
+        name: "userPin",
+        message: "Kindly Enter Your pin: ",
+    },
+    {
+        type: "list",
+        name: "accountType",
+        choices: ["Current", "Saving"],
+        message: "Please Enter Your Account Type: ",
+    },
+    {
+        type: "list",
+        name: "transactionType",
+        choices: ["Fast Cash", "Withdrawl"],
+        message: "Please Enter Your Transaction Type: ",
+        when(answers) {
+            return answers.accountType === "Current";
+        },
+    },
+    {
+        type: "list",
+        name: "amount",
+        choices: [1000, 2000, 5000, 10000, 20000, 50000],
+        message: "Please Select Your Amount",
+        when(answers) {
+            return answers.transactionType === "Fast Cash";
+        },
+    },
+    {
+        type: "list",
+        name: "amount",
+        choices: [1000, 2000, 5000, 10000, 20000, 50000],
+        message: "Please Select Your Withdrawl Amount",
+        when(answers) {
+            return answers.transactionType === "Withdrawl";
+        },
+    },
+]);
+if (answers.userId && answers.userPin) {
+    const balance = Math.floor(Math.random() * 100000);
+    console.log(`Current Balance: ${balance}`);
+    const enteredAmount = answers.amount;
+    if (balance < enteredAmount) {
+        console.log("Insuficiant Balance");
+    }
+    else {
+        const remaining = balance - enteredAmount;
+        console.log(`Your remaining balance is: ${remaining}`);
+    }
+}
